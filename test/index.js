2904f5a4fe024cdf34827e1c8f873706e16216d2
KhouloudK <kalaikhouloud@gmail.com>
2015-12-01 16:36:04 +0100

    catatonic

---
+++ test/index.js
@@ -0,0 +1,11 @@
+import assert from 'assert';
+import { should } from 'chai';
+import catatonicCircle from '../lib';
+
+should(); // Modifies objects prototype to include the 'should' property
+
+describe('catatonic-circle', function () {
+  it('should have a version number!', function () {
+    catatonicCircle.should.have.property('VERSION');
+  });
+});
